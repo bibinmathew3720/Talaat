d6ae5860d1914b1cc4caebe11e52864ff97bae66
Bibin Mathew <bibin.m@codelynks.com>
2017-10-02 11:12:05 +0530

    Project created.

---
+++ Talaat/Talaat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Talaat
+//
+//  Created by Bibin Mathew on 10/2/17.
+//  Copyright © 2017 Talaat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
