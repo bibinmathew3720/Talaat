6da96ff8b13043afc50c01d7dade9f05a8242a6d
Bibin Mathew <bibin.m@codelynks.com>
2017-10-02 11:27:58 +0530

    Pods installation. Folder structure creation.

---
+++ Talaat/Talaat/ViewControllers/BaseViewController/BaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewController.h
+//  Talaat
+//
+//  Created by Bibin Mathew on 10/2/17.
+//  Copyright © 2017 Talaat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewController : UIViewController
+
+@end
