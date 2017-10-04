a0f01b98deae43807ed0ee2dc89909f9a69e08cb
Bibin Mathew <bibin.m@codelynks.com>
2017-10-04 23:25:05 +0530

    Inner page added to project.

---
+++ Talaat/Talaat/ViewControllers/InnerPageVC/InnerPageVC.h
@@ -0,0 +1,13 @@
+//
+//  InnerPageVC.h
+//  Talaat
+//
+//  Created by Bibin Mathew on 10/4/17.
+//  Copyright © 2017 Talaat. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface InnerPageVC : BaseViewController
+
+@end
---
+++ Talaat/Talaat/ViewControllers/InnerPageVC/InnerPageVC.m
@@ -0,0 +1,36 @@
+//
+//  InnerPageVC.m
+//  Talaat
+//
+//  Created by Bibin Mathew on 10/4/17.
+//  Copyright © 2017 Talaat. All rights reserved.
+//
+
+#import "InnerPageVC.h"
+
+@interface InnerPageVC ()
+
+@end
+
+@implementation InnerPageVC
+
+-(void)initView{
+    [super initView];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
