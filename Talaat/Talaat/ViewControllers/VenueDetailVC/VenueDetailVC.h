9b485d5fdf3bb935911a6df9c1769424803bee3f
Bibin Mathew <bibin.m@codelynks.com>
2017-10-09 06:29:58 +0530

    VenueDetail page integration.

---
+++ Talaat/Talaat/ViewControllers/VenueDetailVC/VenueDetailVC.h
@@ -0,0 +1,13 @@
+//
+//  VenueDetailVC.h
+//  Talaat
+//
+//  Created by Bibin Mathew on 10/9/17.
+//  Copyright © 2017 Talaat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VenueDetailVC : UIViewController
+
+@end
