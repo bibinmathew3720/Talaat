7bc9532c580e4a6b326a8b05d9dcb571f44f99f3
Bibin Mathew <bibin.m@codelynks.com>
2017-10-16 08:07:29 +0530

    Offer list populated.

--- Talaat/Talaat/CustomCells/TableCells/InnerPage/OfferCell/OfferCell.m
+++ Talaat/Talaat/CustomCells/TableCells/InnerPage/OfferCell/OfferCell.m
@@ -23,8 +23,21 @@
 
 -(void)setOfferDetail:(id)offerDetail{
     self.offerHeadingLabel.text = [NSString stringWithFormat:@"%@",[offerDetail valueForKey:@"title"]];
-    self.offerDetailLabel.text = [NSString stringWithFormat:@"%@",[offerDetail valueForKey:@"description"]];
+    NSString *validityString = @"";
+    if([offerDetail valueForKey:@"validity"])
+        validityString = [self getValidity:[offerDetail valueForKey:@"validity"]];
+    if(validityString.length>0)
+        validityString = [NSString stringWithFormat:@"*Offer valid till %@th",validityString];
+    self.offerDetailLabel.text = [NSString stringWithFormat:@"%@\n%@",[offerDetail valueForKey:@"description"],validityString];
     [self.offerImageView sd_setImageWithURL:[NSURL URLWithString:[offerDetail valueForKey:@"image_path"]] placeholderImage:[UIImage imageNamed:PlaceholderImageName]];
+}
+
+-(NSString *)getValidity:(NSString *)dateString{
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"yyyy-MM-dd"];
+    NSDate *date = [dateFormatter dateFromString:dateString];
+    [dateFormatter setDateFormat:@"MMM dd"];
+    return [dateFormatter stringFromDate:date];
 }
 
 @end
